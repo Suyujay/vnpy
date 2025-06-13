416b233dca013928d0a52b70e495c9f4acbb516f
suyujay <suyujay@gmail.com>
2025-06-13 16:10:18 +0800

    [Mod] add SubscribeRequest and Exchange imports; integrate IbGateway into main engine

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from vnpy.event import EventEngine
 from vnpy.trader.engine import MainEngine
 from vnpy.trader.ui import MainWindow, create_qapp
+from vnpy.trader.object import SubscribeRequest
+from vnpy.trader.constant import Exchange  # 新增這行
 
+from vnpy_ib import IbGateway
 # from vnpy_ctp import CtpGateway
 # from vnpy_ctastrategy import CtaStrategyApp
 # from vnpy_ctabacktester import CtaBacktesterApp
@@ -13,7 +16,13 @@ def main():
 
     event_engine = EventEngine()
     main_engine = MainEngine(event_engine)
-    
+    main_engine.add_gateway(IbGateway)
+    # req = SubscribeRequest(
+    #     symbol="NQ",   # IB格式: 股票代碼-類型-幣種
+    #     exchange=Exchange.CME         # IB常用交易所
+    # )
+    # main_engine.subscribe(req, "IB")  # "IB"為gateway名稱
+
     # main_engine.add_gateway(CtpGateway)
     # main_engine.add_app(CtaStrategyApp)
     # main_engine.add_app(CtaBacktesterApp)
